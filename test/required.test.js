17a711a7910cc1f2dc7f0f32ce7f6c877978bb52
Kiril Vatev <vatev.1@gmail.com>
2019-02-26 23:19:54 -0500

    switching tests to using syntax legal in node 4

--- test/required.test.js
+++ test/required.test.js
@@ -29,17 +29,17 @@ const files = stdout => {
   return stdout.trim().split('\n').map(s => JSON.parse(s));
 };
 
-it('finds all modules in require chain', async () => {
-  const result = await run('entry1.js');
-
-  expect(files(result.stdout)).to.deep.equal([
-    { type: 'file', id: resolve('fixtures/files/test1.js') },
-    { type: 'file', id: resolve('fixtures/files/test2.js') },
-    { type: 'module', id: resolve('node_modules/resolve-from/index.js') },
-    { type: 'file', id: resolve('fixtures/files/test3.js') },
-    { type: 'builtin', id: 'fs' },
-    { type: 'builtin', id: 'path' },
-    { type: 'builtin', id: 'http' }
-  ]);
-  expect(result.stderr.trim()).to.equal('');
+it('finds all modules in require chain', () => {
+  return run('entry1.js').then((result) => {
+    expect(files(result.stdout)).to.deep.equal([
+      { type: 'file', id: resolve('fixtures/files/test1.js') },
+      { type: 'file', id: resolve('fixtures/files/test2.js') },
+      { type: 'module', id: resolve('node_modules/resolve-from/index.js') },
+      { type: 'file', id: resolve('fixtures/files/test3.js') },
+      { type: 'builtin', id: 'fs' },
+      { type: 'builtin', id: 'path' },
+      { type: 'builtin', id: 'http' }
+    ]);
+    expect(result.stderr.trim()).to.equal('');
+  });
 });
